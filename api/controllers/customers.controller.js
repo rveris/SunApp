97590f858d2827e577463e7ee512cadbdd5b94c7
Ruben Veris <r.veris@ruve.tech>
2017-06-13 01:06:59 +0200

    Start...

---
+++ api/controllers/customers.controller.js
@@ -0,0 +1,18 @@
+module.exports.customersGetAll = function(req, res) {
+    console.log("GET all the customers!");
+    console.log(req.query);
+
+    var offset = 0;
+    var count = 20;
+
+    if (req.query && req.query.offset) {
+        offset = parseInt(req.query.offset, 10);
+    };
+    if (req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
+    };
+
+    var returnData = customerData.slice(offset, offset + count);
+
+    res.json({ returnData });
+};
---
+++ sunapp.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require('./api/routes');
+
+app.set('port', 3000);
+
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('./api', routes);
